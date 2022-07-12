bbfb24dcb8ab2d7cd2aea3affe3716ba99da774c
delegend28 <105609391+delegend28@users.noreply.github.com>
2022-07-11 20:45:06 -0400

    Create common.groovy

---
+++ vars "/common.groovy
@@ -0,0 +1,16 @@
+common.groovy
+def call(String stageName) {
+
+    if ("${stageName}" == "Build")
+        {
+            sh "mvn clean package"
+        }
+    else if ("${stageName}" == "SonarQube Report")
+        {
+            sh "mvn clean sonar:sonar"
+        }
+    else if ("${stageName}" == "Upload into Nexus")
+        {
+            sh "mvn clean deploy"
+        }
+}
